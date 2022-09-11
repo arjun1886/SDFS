e11a928ee477860d63b8750533990ba0bf1cdc40
simrita2 <simrita2@illinois.edu>
2022-09-11 11:30:54 -0500

    Modify tests

--- src/cmd/client/client_test.go
+++ src/cmd/client/client_test.go
@@ -3,27 +3,24 @@ package main
 import (
 	"CS425/cs-425-mp1/src/coordinator"
 	"google.golang.org/grpc"
-	"log"
 	"strconv"
 	"testing"
 	"time"
 )
 
-func establishConnection(t *testing.T) (c coordinator.CoordinatorServiceClient) {
-	var conn *grpc.ClientConn
+func establishConnection(t *testing.T) (c coordinator.CoordinatorServiceClient, conn *grpc.ClientConn) {
 	conn, err := grpc.Dial("fa22-cs425-6602.cs.illinois.edu:8001", grpc.WithInsecure(), grpc.WithTimeout(time.Duration(2000)*time.Millisecond), grpc.WithBlock())
 	if err != nil {
 		t.Fatalf("Did not connect to coordinator: %s", err)
 	}
-	defer conn.Close()
 	c = coordinator.NewCoordinatorServiceClient(conn)
-	return c
+	return c, conn
 }
 
 func TestFrequentPattern(t *testing.T) {
 
-	c := establishConnection(t)
-
+	c, conn := establishConnection(t)
+	defer conn.Close()
 	inputFlag := "-c"
 	inputString := "Mozilla"
 	expectedOutput := []int{269198, 254504, 255377, 257082, 257504, 255625, 254744, 260674, 256399, 251984}
@@ -33,7 +30,6 @@ func TestFrequentPattern(t *testing.T) {
 	t.Logf("Duration of TestFrequentPattern: %d", duration)
 
 	for i := 0; i < len(coordinatorOutput.FileName); i++ {
-		log.Printf("%s\t\t%s\n", coordinatorOutput.FileName[i], coordinatorOutput.Matches[i])
 		intVar, err := strconv.Atoi(coordinatorOutput.Matches[i])
 		if err != nil {
 			t.Logf("Error converting matches to int")
@@ -48,10 +44,11 @@ func TestFrequentPattern(t *testing.T) {
 
 func TestSomewhatFrequentPattern(t *testing.T) {
 
-	c := establishConnection(t)
+	c, conn := establishConnection(t)
+	defer conn.Close()
 
 	inputFlag := "-c"
-	inputString := "iPod; U;" //GET /apps/cart.jsp?appID=5611 HTTP/1.0
+	inputString := "iPod; U;"
 	expectedOutput := []int{9348, 9018, 8823, 9161, 9070, 8934, 9006, 9033, 8996, 9012}
 
 	coordinatorOutput, duration := FetchOutput(inputFlag, inputString, c, 1)
@@ -59,7 +56,6 @@ func TestSomewhatFrequentPattern(t *testing.T) {
 	t.Logf("Duration of TestSomewhatFrequentPattern: %d", duration)
 
 	for i := 0; i < len(coordinatorOutput.FileName); i++ {
-		log.Printf("%s\t\t%s\n", coordinatorOutput.FileName[i], coordinatorOutput.Matches[i])
 		intVar, err := strconv.Atoi(coordinatorOutput.Matches[i])
 		if err != nil {
 			t.Logf("Error converting matches to int")
@@ -74,7 +70,8 @@ func TestSomewhatFrequentPattern(t *testing.T) {
 
 func TestInfrequentPattern(t *testing.T) {
 
-	c := establishConnection(t)
+	c, conn := establishConnection(t)
+	defer conn.Close()
 
 	inputFlag := "-c"
 	inputString := "GET /apps/cart.jsp?appID=5611 HTTP/1.0"
@@ -85,7 +82,6 @@ func TestInfrequentPattern(t *testing.T) {
 	t.Logf("Duration of InfrequentPattern: %d", duration)
 
 	for i := 0; i < len(coordinatorOutput.FileName); i++ {
-		log.Printf("%s\t\t%s\n", coordinatorOutput.FileName[i], coordinatorOutput.Matches[i])
 		intVar, err := strconv.Atoi(coordinatorOutput.Matches[i])
 		if err != nil {
 			t.Logf("Error converting matches to int")
