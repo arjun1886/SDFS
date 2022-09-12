2cfa109172a2dbd9af2e8eb383a9fea3f82edcca
ak85 <ak85@illinois.edu>
2022-09-11 21:48:30 -0500

    Updated comments

--- src/cmd/coordinator/coordinator_server.go
+++ src/cmd/coordinator/coordinator_server.go
@@ -17,7 +17,7 @@ func main() {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
-    // Initialize coordinator structure that implements service interface
+    // Initialize coordinator structure that implements coordinator service interface
 	c := coordinator.Coordinator{}
 
 	grpcServer := grpc.NewServer()
