d964d9ee42d7c3d0566f12cd6106c2d866c2e786
ak85 <ak85@illinois.edu>
2022-09-11 22:06:58 -0500

    Update worker.go

--- src/worker/worker.go
+++ src/worker/worker.go
@@ -6,16 +6,19 @@ import (
 	"strings"
 )
 
+// Worker - Struct that implements the worker service methods from the pb file
 type Worker struct {
 	UnimplementedWorkerServiceServer
 }
 
+// FetchWorkerOutput - Executes the grep command on the worker and fetches its output
 func (w *Worker) FetchWorkerOutput(ctx context.Context, workerInput *WorkerInput) (*WorkerOutput, error) {
 	app := "grep"
 	arg0 := workerInput.Flag
 	arg1 := workerInput.Data
 	arg2 := workerInput.LogFileName
 
+	// Executes OS grep call
 	cmd := exec.Command(app, arg0, arg1, arg2)
 
 	stdout, _ := cmd.Output()
