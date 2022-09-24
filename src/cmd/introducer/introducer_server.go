7b9b8c1ba69db0c1fa88ee48b0b9bb421a023a18
simrita2 <simrita2@illinois.edu>
2022-09-24 13:25:21 -0500

    Adding Introducer

--- src/cmd/introducer/introducer_server.go
+++ src/cmd/introducer/introducer_server.go
@@ -1,1 +1,28 @@
-package introducer
+package main
+
+import (
+	"CS425/cs-425-mp1/src/introducer"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 8001))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Initialize introducer structure that implements introducer service interface
+	c := introducer.Introducer{}
+
+	grpcServer := grpc.NewServer()
+
+	introducer.RegisterIntroducerServiceServer(grpcServer, &c)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %s", err)
+	}
+}
