3740bdf9848683c3598c8e87051f1314247f822c
simrita2 <simrita2@illinois.edu>
2022-11-06 12:33:08 -0600

    Replication and get replica targets functionality

--- src/sdfs_server/sdfs_server.go
+++ src/sdfs_server/sdfs_server.go
@@ -4,6 +4,7 @@ import (
 	"CS425/cs-425-mp1/src/conf"
 	"CS425/cs-425-mp1/src/membership"
 	"errors"
+	"hash/fnv"
 	"io/ioutil"
 	"log"
 	"os"
@@ -53,7 +54,7 @@ func UpdateFileNames() error {
 
 func Put(fileObject conf.FileData) error {
 
-	file := fileObject.FileName
+	file := fileObjectde.FileName
 	fileName := strings.Split(file, ".")[0]
 	fileName = fileName + "_ver_" + strconv.FormatInt(time.Now().Unix(), 10)
 
@@ -125,11 +126,103 @@ func Ls(fileName string) []string {
 	for i := 0; i < len(*members); i++ {
 		fileNames := (*members)[i].FileNames
 		for j := 0; j < len(fileNames); j++ {
-			if fileNames[j] == fileName {
+			if strings.Split(fileNames[j], "_")[0] == fileName {
 				hostNames = append(hostNames, (*members)[i].ProcessId)
 				break
 			}
 		}
 	}
 	return hostNames
+}
+
+func hash(s string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return h.Sum32()
+}
+
+func Replication(file string) error {
+	membershipStruct := membership.Membership{}
+	members := membershipStruct.GetMembers()
+	sdfsServerStruct := sdfsServer{}
+	fileName := strings.Split(file, "_")[0]
+	existingReplicas := Ls(fileName)
+	flag := 0
+	numReplicas := len(existingReplicas)
+
+	if numReplicas == 5 {
+		//for i := 0; i < 5; i++ {
+		//	sdfsServerStruct.put(fileName, existingReplicas[i])
+		//}
+		return nil
+	}
+
+	requiredReplicas := 5 - numReplicas
+
+	mainReplicaIndex := hash(fileName) % uint32(len(*members))
+
+	// See if primary replica already exists
+	for i := 0; i < len(existingReplicas); i++ {
+		if mainReplicaIndex == uint32(existingReplicas[i][14]) {
+			flag = 1
+		}
+	}
+	// If it doesn't exist, find and put in primary replica
+	if flag == 0 {
+		for i := 0; i < len(*members); i++ {
+			if mainReplicaIndex == uint32(members[i][14]) {
+				sdfsServerStruct.put(fileName, members[i])
+			}
+		}
+	}
+
+	// Linearly scan to find and put in next replicas
+	j := (int(mainReplicaIndex) + 1) % len(*members)
+	for requiredReplicas > 0 {
+		if !Contains(existingReplicas, members[j]) {
+			sdfsServerStruct.put(fileName, members[j])
+			requiredReplicas -= 1
+			j = (j + 1) % len(*members)
+		}
+	}
+	return nil
+}
+
+func GetReplicaTargets(file string) []string {
+	membershipStruct := membership.Membership{}
+	members := membershipStruct.GetMembers()
+	// sdfsServerStruct := sdfsServer{}
+	fileName := strings.Split(file, "_")[0]
+	existingReplicas := Ls(fileName)
+	hostNames := existingReplicas
+	numReplicas := len(hostNames)
+	if numReplicas == 5 {
+		return hostNames
+	}
+
+	requiredReplicas := 5 - numReplicas
+	mainReplicaIndex := hash(fileName) % uint32(len(*members))
+	j := (int(mainReplicaIndex)) % len(*members)
+	for requiredReplicas > 0 {
+		if !Contains(existingReplicas, *members[j]) {
+			// sdfsServerStruct.put(fileName, members[j])
+			hostNames = append(hostNames, (*members)[j].ProcessId)
+			requiredReplicas -= 1
+			j = (j + 1) % len(*members)
+		}
+	}
+
+	return hostNames
+
+}
+
+func Contains(list []string, element string) bool {
+	var result bool = false
+	for _, x := range list {
+		if x == element {
+			result = true
+			break
+		}
+	}
+	return result
 }
